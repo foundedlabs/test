bf130e00be585538364422f39b13c22644d00bbd
Milko Mihailov <milko.mihailov@sofyma.com>
2015-11-13 14:03:44 +0200

    initial commit

---
+++ config/ttwitter.php
@@ -0,0 +1,19 @@
+<?php
+
+// You can find the keys here : https://apps.twitter.com/
+
+return [
+	'debug' => false,
+	'API_URL' => 'api.twitter.com',
+	'UPLOAD_URL' => 'upload.twitter.com',
+	'API_VERSION' => '1.1',
+	'AUTHENTICATE_URL' => 'https://api.twitter.com/oauth/authenticate',
+	'AUTHORIZE_URL' => 'https://api.twitter.com/oauth/authorize',
+	'ACCESS_TOKEN_URL' => 'https://api.twitter.com/oauth/access_token',
+	'REQUEST_TOKEN_URL' => 'https://api.twitter.com/oauth/request_token',
+	'USE_SSL' => true,
+	'CONSUMER_KEY' => 'Fh2aQMpLBQUfwAI07btpTzfA4',
+	'CONSUMER_SECRET' => 'SXSdOh3yChRayxUk8V4gfNO7zhTfOeqr1LPhcpAhLfqobt6B08',
+	'ACCESS_TOKEN' => '90764511-lePcBF25iTNt5Q1jqlTBqBEZ0i6VyOjTBYVZs3vhO',
+	'ACCESS_TOKEN_SECRET' => 'uOBz9DctWmysSFTqJPWZsD1xhIWcDhPelU849ZiV9LoGU',
+];
